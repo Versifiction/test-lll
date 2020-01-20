9eda173fd5c8bd5705f298d93d7f781bb3955b23
Versifiction <marc_charpentier@hotmail.fr>
2020-01-20 20:54:58 +0100

    suite faite à la maison

--- server.js
+++ server.js
@@ -1,49 +1,60 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const leboncoin = require("leboncoin-api");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const nominatim = require("nominatim");
 
-app.use(cors())
+app.use(cors());
 app.use(
-    bodyParser.urlencoded({
-      extended: false
-    })
-  );
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Bienvenue sur l'API du Test LLL de Marc Charpentier")
-})
+  res.send("Bienvenue sur l'API du Test LLL de Marc Charpentier");
+});
 
-app.get("/info", (req, res) => {
-    console.log("route info")
-    console.log("body ", req.body)
-    console.log("req ", req)
+app.post("/getInfos", (req, res) => {
+  console.log("/getInfos");
+  console.log("body ", req.body);
+  // console.log("req ", req)
 
+  if (req.body.min || req.body.max) {
     var search = new leboncoin.Search()
-    .setPage(1)
-    .setQuery("renove")
-    .setFilter(leboncoin.FILTERS.PARTICULIER)
-    .setCategory("locations")
-    .setRegion("ile_de_france")
-    .addSearchExtra("price", {min: 1500, max: 2000}) // will add a range of price
-    .addSearchExtra('furnished', ["1", "Non meublé"]);
+      .setPage(1)
+      .setQuery("renove")
+      .setFilter(leboncoin.FILTERS.PARTICULIER)
+      .setCategory("locations")
+      .setRegion("ile_de_france")
+      .addSearchExtra("price", { min: req.body.min, max: req.body.max }) // will add a range of price
+      .addSearchExtra("furnished", ["1", "Non meublé"]);
 
-    search.run().then(function (data) {
+    search.run().then(
+      function(data) {
         // console.log(data.page); // the current page
         // console.log(data.nbResult); // the number of results for this search
         // console.log(data.results); // the array of results
-        data.results[0].getDetails().then(function (details) {
+        data.results[0].getDetails().then(
+          function(details) {
             // console.log(details); // the item 0 with more data such as description, all images, author, ...
-            res.send(data.results)
-            // console.log("results ", data.results.splice(0, 1))
-        }, function (err) {
+            res.send(data.results);
+            //   console.log("results ", data.results.splice(0, 1));
+          },
+          function(err) {
             console.error(err);
-        });
-    }, function (err) {
+          }
+        );
+      },
+      function(err) {
         console.error(err);
-    });
-})
+      }
+    );
+  }
+});
 
-app.listen(5000, () => { console.log("Le serveur tourne sur le port 5000")})
+app.listen(5000, () => {
+  console.log("Le serveur tourne sur le port 5000");
+});
--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Map from 'pigeon-maps'
-import Marker from 'pigeon-marker'
-import Overlay from 'pigeon-overlay'
+import Map from "pigeon-maps";
+import Marker from "pigeon-marker";
+import Overlay from "pigeon-overlay";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [minimumPrice, setMinimumPrice] = useState(0)
-  const [maximumPrice, setMaximumPrice] = useState(0)
-  const [sideNavActive, setSideNavActive] = useState(true)
-  const [results, setResults] = useState([])
+  const [city, setCity] = useState();
+  const [minimumPrice, setMinimumPrice] = useState(0);
+  const [maximumPrice, setMaximumPrice] = useState(0);
+  const [longitude, setLongitude] = useState(2.4);
+  const [latitude, setLatitude] = useState(47.0833);
+  const [sideNavActive, setSideNavActive] = useState(true);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log("min ", minimumPrice)
-    console.log("max ", maximumPrice)
-    console.log("results ", results)
-  }, [minimumPrice, maximumPrice, results])
+    console.log("city ", city);
+    console.log("longitude ", longitude);
+    console.log("latitude ", latitude);
+  }, [city, longitude, latitude]);
 
   async function getResults(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const info = {
-      min: minimumPrice,
-      max: maximumPrice
-    }
+      min: Number(minimumPrice),
+      max: Number(maximumPrice)
+    };
 
-    console.log("info ", info)
+    console.log("info ", info);
 
     try {
-      const data = await axios("http://localhost:5000/info", info)
-      console.log("data ", data)
+      const data = await axios.post("http://localhost:5000/getinfos", info);
+      console.log("data ", data);
 
-      setResults(data.data)
-      console.log("results ", data.data)
+      setResults(data.data);
+      console.log("results ", data.data);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
-
-  function toggleSideNav() {
-    setSideNavActive(!sideNavActive);
+  function resetFields() {
+    setMinimumPrice(0);
+    setMaximumPrice(0);
+    setLongitude(2.4);
+    setLatitude(47.0833);
+    setCity();
+    setResults([]);
   }
 
   return (
-    <div className="App" style={{ display: "flex", flexWrap: "nowrap"}}>
-      <Map center={[47.0833, 2.4]} defaultCenter={[47.0833, 2.4]} zoom={6} width={1900} height={600}></Map>
+    <div
+      className="App"
+      style={{ display: "flex", flexWrap: "nowrap", position: "relative" }}
+    >
+      <Map center={[latitude, longitude]} zoom={6} width={1900} height={1080}>
+        {city && (
+          <Marker
+            anchor={[latitude, longitude]}
+            payload={1}
+            onClick={({ event, anchor, payload }) => {}}
+          />
+        )}
+      </Map>
       <ul
         id="slide-out"
         className="sidenav dark"
@@ -58,50 +76,142 @@ function App() {
           style={{ position: "relative", height: "100%" }}
         >
           <div className="container" style={{ paddingTop: "20px" }}>
-          <div className="row"><form onSubmit={getResults}><div className="row">
-          <div className="input-field col s12">
-            <input
-              id="minimum-price"
-              type="number"
-              name="minimum"
-              placeholder="Entrez votre prix minimum"
-              value={minimumPrice}
-              onChange={(e) => setMinimumPrice(e.target.value)}
-              style={{ backgroundColor: "transparent !important" }}
-              required
-            />
-            <label htmlFor="email">Prix minimum</label>
-          </div></div>
-          <div className="row">
-          <div className="input-field col s12">
-            <input
-              id="maximum-price"
-              type="number"
-              name="maximum"
-              placeholder="Entrez votre prix maximum"
-              value={maximumPrice}
-              onChange={(e) => setMaximumPrice(e.target.value)}
-              style={{ backgroundColor: "transparent !important" }}
-              required
-            />
-            <label htmlFor="email">Prix maximum</label>
-          </div></div>
-          <div className="row">
-          <div className="col s12" style={{ textAlign: "center"}}>
-          <button className="btn waves-effect waves-light" type="submit" name="action">Rechercher
-            <i className="material-icons right">send</i>
-          </button>
-          </div></div>
-        </form>
+            <div className="row">
+              <div style={{ marginBottom: "50px" }}>
+                Veuillez rentrer un prix minimum ou un prix maximum, et obtenez
+                les 5 premiers résultats d'annonces Leboncoin correspondant à
+                ces critères :
+                <ul>
+                  <li>catégorie Locations</li>
+                  <li>en ïle-de-France</li>
+                  <li>non meublé</li>
+                  <li>
+                    avec un prix compris entre{" "}
+                    <span style={{ color: "#0CD0FC" }}>{minimumPrice}</span>
+                    &nbsp;€ et&nbsp;
+                    <span style={{ color: "#0CD0FC" }}>{maximumPrice}</span>
+                    &nbsp;€
+                  </li>
+                </ul>
+              </div>
+              <form onSubmit={getResults} autoComplete="off" method="post">
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input
+                      id="minimum-price"
+                      type="number"
+                      name="minimumPrice"
+                      placeholder="Entrez votre prix minimum"
+                      value={minimumPrice}
+                      onChange={e => setMinimumPrice(e.target.value)}
+                      style={{ backgroundColor: "transparent !important" }}
+                      required
+                    />
+                    <label htmlFor="email">Prix minimum</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input
+                      id="maximum-price"
+                      type="number"
+                      name="maximumPrice"
+                      placeholder="Entrez votre prix maximum"
+                      value={maximumPrice}
+                      onChange={e => setMaximumPrice(e.target.value)}
+                      style={{ backgroundColor: "transparent !important" }}
+                      required
+                    />
+                    <label htmlFor="email">Prix maximum</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col s12" style={{ textAlign: "center" }}>
+                    <button
+                      className="btn waves-effect waves-light"
+                      type="submit"
+                      name="action"
+                    >
+                      Rechercher
+                      <i className="material-icons right">send</i>
+                    </button>
+                  </div>
+                </div>
+                {results.length > 0 && (
+                  <div className="row">
+                    <div
+                      className="col s12"
+                      style={{ display: "flex", justifyContent: "center" }}
+                    >
+                      <i
+                        className="material-icons right"
+                        style={{ cursor: "pointer" }}
+                        onClick={resetFields}
+                        title="Réinitialiser la recherche"
+                      >
+                        autorenew
+                      </i>
+                    </div>
+                  </div>
+                )}
+              </form>
+            </div>
+          </div>
         </div>
-        </div>
-       </div>
       </ul>
       {results.length > 0 && (
-        <div id="results">
+        <div
+          id="results"
+          style={{
+            position: "absolute",
+            bottom: "0",
+            width: "calc(100% - 300px)",
+            display: "flex",
+            flexWrap: "wrap",
+            backgroundColor: "#414141"
+          }}
+        >
           {results.slice(0, 5).map(result => (
-            <div className="result" key={result.id} style={{ display: "flex", flexGrow: "1", backgroundColor: "#414141"}}>
-              {result.location.city_label}
+            <div
+              className="result"
+              key={result.id}
+              style={{
+                color: "white",
+                textAlign: "center",
+                padding: "20px",
+                width: "20%"
+              }}
+            >
+              <a href={result.link} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={result.images[0]}
+                  alt={`Annonce ${result.id}`}
+                  title={`Annonce ${result.id}`}
+                  style={{ height: "100px" }}
+                />
+                <p style={{ fontWeight: "bold", color: "white" }}>
+                  {result.title}
+                </p>
+                <p style={{ color: "#bbb" }}>{result.location.city_label}</p>
+              </a>
+              <div
+                style={{
+                  color: "#0CD0FC",
+                  cursor: "pointer",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+                onClick={e => {
+                  setCity(result.location.city_label);
+                  setLongitude(result.location.lng);
+                  setLatitude(result.location.lat);
+                  // getAnchor(e);
+                }}
+              >
+                <p>Voir sur la carte</p>
+                <i className="material-icons right">place</i>
+              </div>
             </div>
           ))}
         </div>
